546b5db623f5a6eaa7191be307fa42456c8af4ed
Nisha Chandramoorthy <nishac@mit.edu>
2020-11-21 18:50:26 -0500

    Add function to plot density

--- plot_density.jl
+++ plot_density.jl
@@ -8,27 +8,30 @@ function prob(n,s)
 	dx = 1/n
 	nsteps = 50000000
 	x = rand()
+	avg = 0.
 	for i = 1:nsteps
 		x = f(x, s)
+		avg += x/nsteps
 		ind = floor(Int64, div(x, dx))
 		dens[ind+1] += n/nsteps
 	end
+	println(avg)
 	return dens
 end
 fig = figure(figsize=(10,5))
 ax = fig.add_subplot(111)
 n = 50
-s = [0.97,0.5]
+s = [1.0,0.5]
 dens = prob(n,s)
 dx = 1/n
 ax.plot(dx*(1:n), dens,label="s=0.5",lw=2.5)
-s = [0.97,0.6]
+s = [1.0,0.6]
 dens = prob(n,s)
 ax.plot(dx*(1:n), dens,label="s=0.6",lw=2.5)
-s = [0.97,0.7]
+s = [1.0,0.7]
 dens = prob(n,s)
 ax.plot(dx*(1:n), dens,label="s=0.7",lw=2.5)
-s = [0.97,0.8]
+s = [1.0,0.8]
 dens = prob(n,s)
 ax.plot(dx*(1:n), dens,label="s=0.8",lw=2.5)
 
